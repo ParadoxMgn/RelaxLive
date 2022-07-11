288a71c39637398f6786a9cc89ca99218d4eac5a
Eugene Shumkov <radoxmgn@gmail.com>
2022-07-11 20:04:23 +0300

    Create maskPhones

---
+++ src/modules/maskPhone.js
@@ -0,0 +1,41 @@
+export const maskPhone = () => {
+  const elems = document.querySelectorAll('input[name=phone]');
+
+  const mask = function (e) {
+    const keyCode = e.keyCode;
+    const template = '+7 (___) ___-__-__';
+    const def = template.replace(/\D/g, "");
+    const val = this.value.replace(/\D/g, "");
+
+    let i = 0;
+    let newValue = template.replace(/[_\d]/g, a => i < val.length ? val.charAt(i++) || def.charAt(i) : a);
+
+    i = newValue.indexOf("_");
+
+    if (i !== -1) {
+      newValue = newValue.slice(0, i);
+    }
+
+    let reg = template.substr(0, this.value.length)
+      .replace(/_+/g, a => "\\d{1," + a.length + "}")
+      .replace(/[+()]/g, "\\$&");
+
+    reg = new RegExp("^" + reg + "$");
+
+    if (!reg.test(this.value) || this.value.length < 5 || keyCode > 47 && keyCode < 58) {
+      this.value = newValue;
+    }
+
+    if (e.type === "blur" && this.value.length < 5) {
+      this.value = "";
+    }
+
+  };
+
+  for (const elem of elems) {
+    elem.addEventListener("input", mask);
+    elem.addEventListener("focus", mask);
+    elem.addEventListener("blur", mask);
+  }
+
+};
