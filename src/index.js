eecc73f5983632f030c99489712afe694039ac44
Eugene Shumkov <radoxmgn@gmail.com>
2022-07-11 20:03:51 +0300

    Create maskPhone

--- src/index.js
+++ src/index.js
@@ -2,8 +2,10 @@ import { phoneList } from './modules/phoneList';
 import { burgerMenu } from './modules/burgerMenu';
 import { smoothScroll } from './modules/smoothScroll';
 import { modals } from './modules/modals';
+import { maskPhone } from './modules/maskPhone';
 
 phoneList();
 burgerMenu();
 smoothScroll();
 modals();
+maskPhone();
